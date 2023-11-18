80f584a4ff301362d3eb52eaf9d5ddc3973f97ab
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-11-18 15:27:43 +0500

    spline add

--- app/page.js
+++ app/page.js
@@ -6,6 +6,12 @@ export default function Home() {
     <main className="bg-[#F0F0F0] min-h-screen w-screen grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  grid xl:grid-cols-6 gap-4 md:gap-8 xl:gap-10 p-5 md:p-10 xl:p-20">
       <Link
         className="border overflow-hidden transition-all ease-linear duration-100 hover:shadow-lg border-gray-200 hover:border-blue-500 bg-white font-bold backdrop-blur-xl rounded-lg text-center text-black h-fit"
+        href={"/spline"}>
+        <img className="w-full h-[100px] object-cover" src={'/spline.png'}/>
+         <p className=" px-3 py-2">Cursor tracker</p>
+      </Link>
+      <Link
+        className="border overflow-hidden transition-all ease-linear duration-100 hover:shadow-lg border-gray-200 hover:border-blue-500 bg-white font-bold backdrop-blur-xl rounded-lg text-center text-black h-fit"
         href={"/sculpture"}>
         <img className="w-full h-[100px] object-cover" src={'/sculpture.png'}/>
          <p className=" px-3 py-2">Scroll 3d</p>
