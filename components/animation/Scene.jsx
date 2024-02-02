a33cb322ee757af48dbc5dfc0368e0cba7516ecf
GEORGIY ASLANYAN <13jeff07@gmail.com>
2024-02-02 17:37:39 +0500

    upd animation

--- components/animation/Scene.jsx
+++ components/animation/Scene.jsx
@@ -9,16 +9,17 @@ const Scene = () => {
 
   return (
     <div className="w-screen h-screen relative">
-      <div className="absolute flex gap-3 p-3 z-50 top-5 left-5 rounded-xl bg-black border border-neutral-800 text-white">
+      {/* <div className="absolute flex gap-3 p-3 z-50 top-5 left-5 rounded-xl bg-black border border-neutral-800 text-white">
         <button className={`w-10 h-10 flex items-center justify-center rounded-lg ${active === 0 ? "bg-blue-500" : "bg-gray-700"}`} onClick={() => setActive(0)}>1</button>
         <button className={`w-10 h-10 flex items-center justify-center rounded-lg ${active === 1 ? "bg-blue-500" : "bg-gray-700"}`} onClick={() => setActive(1)}>2</button>
         <button className={`w-10 h-10 flex items-center justify-center rounded-lg ${active === 2 ? "bg-blue-500" : "bg-gray-700"}`} onClick={() => setActive(2)}>3</button>
         <button className={`w-10 h-10 flex items-center justify-center rounded-lg ${active === 3 ? "bg-blue-500" : "bg-gray-700"}`} onClick={() => setActive(3)}>4</button>
-      </div>
+      </div> */}
     <Suspense fallback={<div className="h-screen w-full flex justify-center items-center"> <PuffLoader color={'white'} /> </div>}>
-     {active === 0 ? <div className="w-screen h-screen">
-          <Spline scene="https://prod.spline.design/RU06nEEZQSIkWxSi/scene.splinecode"  />
-      </div> : active === 1 ?
+      <div className="w-screen h-screen">
+          <Spline scene="https://prod.spline.design/LLXph7-9uugBtBcj/scene.splinecode"  />
+      </div>
+       {/* : active === 1 ?
       <div className="w-screen h-screen">
           <Spline scene="https://prod.spline.design/BuhdgQuqS8MNecrq/scene.splinecode"  />
       </div> : active === 2 ?
@@ -27,7 +28,7 @@ const Scene = () => {
       </div> :
       <div className="w-screen h-screen">
           <Spline scene="https://prod.spline.design/mIfy6Zpe5XFpBM98/scene.splinecode"  />
-      </div>}
+      </div>} */}
     </Suspense>
     </div>
   );
